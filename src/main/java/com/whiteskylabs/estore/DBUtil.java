afa3200b7bcb494d75d715c49553d0238f86761d
Kalyan <kalyan.menda@whiteskylabs.com>
2014-07-25 17:18:21 +0530

    Instead loading properties again injected 'sql.dir.location' to a spring bean
    Signed-off-by: Kalyan <kalyan.menda@whiteskylabs.com>

--- src/main/java/com/whiteskylabs/estore/DBUtil.java
+++ src/main/java/com/whiteskylabs/estore/DBUtil.java
@@ -24,6 +24,7 @@ public class DBUtil implements Callable {
 	private final static Logger log = Logger.getLogger(DBUtil.class.getName());
 
 	public GenericDbConfig derbyConnector;
+	private String sqlDirLocation;
 
 	public GenericDbConfig getDerbyConnector() {
 		return derbyConnector;
@@ -31,6 +32,14 @@ public class DBUtil implements Callable {
 
 	public void setDerbyConnector(GenericDbConfig derbyConnector) {
 		this.derbyConnector = derbyConnector;
+	}
+	
+	public String getSqlDirLocation() {
+		return sqlDirLocation;
+	}
+
+	public void setSqlDirLocation(String sqlDirLocation) {
+		this.sqlDirLocation = sqlDirLocation;
 	}
 
 	/**
@@ -100,27 +109,30 @@ public class DBUtil implements Callable {
 	private ArrayList<String> getSQLFilePaths() throws Exception {
 		Properties prop = new Properties();
 		ArrayList<String> sqlPaths = new ArrayList<String>();
+
 		// Location of folder name "db-queries"
-		
 		String dirPath = getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
-		//loading properties file				
-		prop.load(new FileInputStream(dirPath+"info.properties"));
 		
-		String sqlDirLoc = prop.getProperty("sql.dir.location");
+		String sqlDirLoc = getSqlDirLocation();
+		
 		// pointing to "db-queries"
 		File dirObject = new File(dirPath+sqlDirLoc);
+		
 		// List of childs(i.e files and folders from db-queries directory)
 		String[] child = dirObject.list();
 		
 		// check for 'db-queries' is directory or not
 		if (dirObject.isDirectory()) {
+			
 			//digging into folder for sql files
 			if (child.length == 0) {
 				throw new Exception("No SQL files available to setup DB");
 			} else {
+			
 				//Reading each file
 				for (String file : child) {
 					File fileObject = new File(dirPath + sqlDirLoc +"/"+ file);
+				
 					//Restricts to read only .sql files 
 					if (fileObject.isFile()	&& fileObject.getName().endsWith(".sql")) {
 						sqlPaths.add(fileObject.getPath());
